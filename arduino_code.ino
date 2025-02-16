80bdce71a5166586d1e0bb8d4dca720501f6ca26
Jamie <jdenley@umass.edu>
2025-02-16 09:40:43 -0500

    working mvp

--- arduino_code.ino
+++ arduino_code.ino
@@ -2,24 +2,22 @@ int ButtonValue;
 int buzzer  = 11;
 int button = 8;
 int buttonPressCount;
-
 void setup() {
   Serial.begin(9600);
+  // Serial.println("Welcome to Habit Rabbit!");
   pinMode(button, INPUT_PULLUP);
   pinMode(buzzer, OUTPUT); 
 }
-
 void loop() {
   ButtonValue = digitalRead(button);
   if (ButtonValue == LOW) {  // Button is pressed
-    tone(buzzer, 564);
-    digitalWrite(buzzer, HIGH);  // Play 
     buttonPressCount+=1;
     Serial.println(buttonPressCount);
+    tone(buzzer, 564);
+    digitalWrite(buzzer, HIGH);  // Play 
     delay(500);
   } else {
     noTone(buzzer);
     digitalWrite(buzzer, LOW);  // Stop the buzzer when button is released
   }
 }
-
